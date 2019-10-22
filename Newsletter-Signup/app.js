a1182d98855836ebae884effc6de24d79db8abc4
TakiL <46074633+TakiL@users.noreply.github.com>
2019-10-23 01:00:42 +0200

    Added Mailchimp integration via API

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,12 +1,62 @@
 //jshint esversion:6
 
 const express = require('express');
-const bodyParser=require('body-parser');
+const bodyParser = require('body-parser');
 const request = require('request');
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
 
-app.listen(3000, function(){
+})
+
+app.post("/", function (req, res) {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+
+    var data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName,
+            }
+        }]
+    };
+
+    var jsonData = JSON.stringify(data);
+    
+
+    var options = {
+        url: "https://us20.api.mailchimp.com/3.0/lists/f5c5581665",
+        method: "POST",
+        headers: {
+            "Authorization": "lasalot1 48be7a42bd32df1bcd51f542695c5cee-us20"
+        },
+        body: jsonData
+
+    };
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(response.statusCode);
+        };
+        
+    });
+})
+
+app.listen(3000, function () {
     console.log("Server is running on port 3000")
 })
+
+//API KEY "48be7a42bd32df1bcd51f542695c5cee-us20"
+
+//list id: f5c5581665
