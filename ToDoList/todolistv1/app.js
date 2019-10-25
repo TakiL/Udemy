1a7a331cf1a24448793dfe14c18617ce4015c88d
TakiL <46074633+TakiL@users.noreply.github.com>
2019-10-26 01:23:03 +0200

    Finished To do list without CSS and using EJS

--- ToDoList/todolistv1/app.js
+++ ToDoList/todolistv1/app.js
@@ -1,23 +1,32 @@
 //jshint esversion:6
 
-const express = require ('express');
-const bodyParser = require ('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express();
+const app = express(); // declare app first
+var items = ["Buy Food", "Eat Food", "Cook Food"];
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
-app.get("/", function(req,res){
-    
-var today = new Date();
-var currentDay= today.getDay();
 
-if (currentDay === 6 || currentDay === 0) {
-    res.write("<h1>Yaaay it is the weekend</h1>");
-} else {
-   res.sendFile(__dirname + "/index.html")
-}
+app.get("/", function (req, res) {
+
+    var options = {weekday: 'long', year: 'numeric', month:'long', day:'numeric'};
+    var today = new Date();
+    var day = today.toLocaleDateString("en-US", options);
+
+
+    res.render("list", {
+        kindOfDay: day, newListItems: items
+    });
 })
 
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server running on port 3000");
-});
+}); 
