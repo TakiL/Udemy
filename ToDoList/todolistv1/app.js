f66c005936d104243511635fe1bca6f35a2c1d9e
LaszloT <laszlo.takacs9307@gmail.com>
2019-10-28 16:56:14 +0100

    Todolist progress

--- ToDoList/todolistv1/app.js
+++ ToDoList/todolistv1/app.js
@@ -4,27 +4,42 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express(); // declare app first
-var items = ["Buy Food", "Eat Food", "Cook Food"];
+let items = ["Buy Food", "Eat Food", "Cook Food"];
+let workItems= [];
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
+
 
 
 app.get("/", function (req, res) {
 
-    var options = {weekday: 'long', year: 'numeric', month:'long', day:'numeric'};
-    var today = new Date();
-    var day = today.toLocaleDateString("en-US", options);
+    let options = {weekday: 'long', year: 'numeric', month:'long', day:'numeric'};
+    let today = new Date();
+    let day = today.toLocaleDateString("en-US", options);
 
 
     res.render("list", {
-        kindOfDay: day, newListItems: items
+        listTitle: day, newListItems: items
     });
 })
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
     items.push(item);
     res.redirect("/");
+    console.log(req.body);
+});
+
+
+app.get("/work", function(req,res){
+    res.render("list", { listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req,res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
 })
 
 app.listen(3000, function () {
