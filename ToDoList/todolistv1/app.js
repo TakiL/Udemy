eadb07e1a4c8d40147622e85c9f820317c4c7fd7
TakiL <46074633+TakiL@users.noreply.github.com>
2019-10-29 20:10:49 +0100

    final version of todolist

--- ToDoList/todolistv1/app.js
+++ ToDoList/todolistv1/app.js
@@ -2,11 +2,15 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
+
 
 const app = express(); // declare app first
-let items = ["Buy Food", "Eat Food", "Cook Food"];
-let workItems= [];
-app.use(bodyParser.urlencoded({extended: true}));
+const items = ["Buy Food", "Eat Food", "Cook Food"];
+const workItems = [];
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
@@ -14,41 +18,45 @@ app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
 
-    let options = {weekday: 'long', year: 'numeric', month:'long', day:'numeric'};
-    let today = new Date();
-    let day = today.toLocaleDateString("en-US", options);
-
-
+    const day = date.getDate();
     res.render("list", {
-        listTitle: day, newListItems: items
+        listTitle: day,
+        newListItems: items
     });
 })
 
-app.post("/", function(req, res){
-    let item = req.body.newItem;
+app.post("/", function (req, res) {
+    const item = req.body.newItem;
     if (req.body.list === "Work") {
         workItems.push(item);
         res.redirect("/work");
     } else {
 
-    
-    items.push(item); 
-    res.redirect("/");
-};
-    
+
+        items.push(item);
+        res.redirect("/");
+    };
+
 });
 
 
-app.get("/work", function(req,res){
-    res.render("list", { listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });
 });
 
-app.post("/work", function(req,res){
-    let item = req.body.newItem;
+app.post("/work", function (req, res) {
+    const item = req.body.newItem;
     workItems.push(item);
     res.redirect("/work");
 })
 
+app.get("/about", function (req, res) {
+    res.render("about");
+})
+
 app.listen(3000, function () {
     console.log("Server running on port 3000");
-}); 
+});
