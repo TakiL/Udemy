31a9cf97067ffaceaae85410c97032171e646cd6
Taki <taki@pop-os.localdomain>
2019-11-11 14:03:12 +0100

    test

--- Blog/app.js
+++ Blog/app.js
@@ -3,10 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash")
+const _ = require("lodash");
 
 const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+  "Tacos vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
@@ -25,7 +25,7 @@ app.use(
 app.use(express.static("public"));
 
 //Home route
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.render("home", {
     StartingContent: homeStartingContent,
     posts: posts
@@ -33,30 +33,29 @@ app.get("/", function (req, res) {
 });
 
 //About route
-app.get("/about", function (req, res) {
+app.get("/about", function(req, res) {
   res.render("about", {
     about: aboutContent
   });
 });
 
 //Contact route
-app.get("/contact", function (req, res) {
+app.get("/contact", function(req, res) {
   res.render("contact", {
     contact: contactContent
   });
 });
 
 //Compose route
-app.get("/compose", function (req, res) {
+app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
 //posts route dynamic topic
-app.get("/posts/:postName", function (req, res) {
+app.get("/posts/:postName", function(req, res) {
   const requestedTitle = _.lowerCase(req.params.postName);
 
-
-  posts.forEach(function (post) {
+  posts.forEach(function(post) {
     const storedTitle = _.lowerCase(post.title);
 
     if (requestedTitle === storedTitle) {
@@ -65,18 +64,12 @@ app.get("/posts/:postName", function (req, res) {
         title: post.title,
         content: post.content
       });
-
-    };
+    }
   });
-
-
 });
 
-
-
-
 //fuction for a post request coming from compose.ejs
-app.post("/compose", function (req, res) {
+app.post("/compose", function(req, res) {
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody
@@ -85,6 +78,6 @@ app.post("/compose", function (req, res) {
   res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
