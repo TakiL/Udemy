c9018a4936f0e36f2a2c556ccedb549bec16f64a
Laszlo Takacs <laszlo@letmewebyou.com>
2019-11-27 17:53:54 +0100

    Added final encryption

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -4,12 +4,19 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10; // Salting rounds for bcrypt
+
 
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -20,67 +27,76 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-  excludeFromEncryption: ["email"]
-}); /// has to be sooner than mongoose model
-
 const User = new mongoose.model("User", userSchema);
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res) {
+app.get("/login", function (req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req, res) {
+app.get("/register", function (req, res) {
   res.render("register");
 });
 
 //POST REQUESTS
 
-app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+app.post("/register", function (req, res) {
+
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+
   });
 
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
 });
 
-app.post("/login", function(req, res) {
+app.post("/login", function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({ email: username }, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
+  User.findOne({
+      email: username
+    },
+    function (err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          bcrypt.compare(password, foundUser.password, function (err, result) {
+            if (result === true) {
+              res.render("secrets");
+            }
+          });
         }
       }
     }
-  });
+  );
+});
+
+app.get("/logout", function (req, res) {
+  res.redirect("/");
 });
 
 /////////////////////////// API GET CALL TO SEE USERS //////////////////////////////////////////////
-app.get("/getusers", function(req, res) {
-  User.find(function(err, foundUsers) {
+app.get("/getusers", function (req, res) {
+  User.find(function (err, foundUsers) {
     res.send(foundUsers);
   });
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("App has been started");
 });
